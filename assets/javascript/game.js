1be62430bc076c2e476f8557b70ac8768458d43b
Kenneth Woods <kwoods392@gmail.com>
2020-09-24 16:27:05 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,16 +5,22 @@ let teamtwoshootbutton = document.querySelector("#teamtwo-shoot-button");
 const teamonenumshots = document.querySelector("#teamone-numshots");
 const numresets = document.querySelector("#num-resets");
 const teamtwonumshots = document.querySelector("#teamtwo-numshots");
+const teamOneNumGoals = document.querySelector("#teamone-numgoals");
+const teamTwoNumGoals = document.querySelector("#teamtwo-numgoals");
 
 let teamoneshotstaken = 0;
 let teamtwoshotstaken = 0;
 let shotsreset = 0;
+let firstTeamGoals = 0;
+let secondTeamGoals = 0;
 
 teamoneshootbutton.addEventListener("click", function () {
   teamoneshotstaken = teamoneshotstaken + 1;
   teamonenumshots.innerHTML = teamoneshotstaken;
   if (Math.random() * 100 > 50) {
     console.log("Goals!");
+    firstTeamGoals = firstTeamGoals + 1;
+    teamOneNumGoals.innerHTML = firstTeamGoals;
   }
 });
 
@@ -23,6 +29,8 @@ teamtwoshootbutton.addEventListener("click", function () {
   teamtwonumshots.innerHTML = teamtwoshotstaken;
   if (Math.random() * 100 > 50) {
     console.log("Goals!");
+    secondTeamGoals = secondTeamGoals + 1;
+    teamTwoNumGoals.innerHTML = secondTeamGoals;
   }
 });
 
@@ -30,9 +38,17 @@ resetbutton.addEventListener("click", function () {
   shotsreset = shotsreset + 1;
   numresets.innerHTML = shotsreset;
 
+  teamOneNumGoals.innerHTML = firstTeamGoals;
+
+  teamTwoNumGoals.innerHTML = secondTeamGoals;
+
   teamoneshotstaken = 0;
   teamtwoshotstaken = 0;
+  firstTeamGoals = 0;
+  secondTeamGoals = 0;
 
   teamtwonumshots.innerHTML = teamtwoshotstaken;
   teamonenumshots.innerHTML = teamoneshotstaken;
+  teamOneNumGoals.innerHTML = firstTeamGoals;
+  teamTwoNumGoals.innerHTML = secondTeamGoals;
 });
